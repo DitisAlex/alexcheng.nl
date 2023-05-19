6c23b66cda6942e6a50408a53e835187332a34d7
Alex Cheng <alexcheng0314@gmail.com>
2023-05-19 15:03:50 +0200

    Implemented Emailjs for working form

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { AiFillMail } from "react-icons/ai";
-import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import { FaLinkedinIn, FaGithub, FaTelegramPlane } from "react-icons/fa";
+import { HiInformationCircle } from "react-icons/hi";
 import contactImg from "../public/assets/misc/contact.jpg";
+import emailjs from "@emailjs/browser";
+import { Alert } from "flowbite-react";
 
 const Contact = () => {
+  const form = useRef();
+  const [showAlert, setShowAlert] = useState(false);
+  const [showError, setShowError] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_SERVICE_ID,
+        process.env.NEXT_PUBLIC_TEMPLATE_ID,
+        form.current,
+        process.env.NEXT_PUBLIC_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          if (result) {
+            e.target.reset();
+            setShowAlert(true);
+          }
+        },
+        (error) => {
+          console.log(error);
+          e.target.reset();
+          setShowError(true);
+        }
+      );
+  };
+
   return (
     <div
       id="contact"
@@ -19,7 +51,11 @@ const Contact = () => {
         <div className="col-span-3 lg:col-span-2 w-full h-full shadow-md shadow-gray-400 rounded-xl p-4">
           <div className="lg:p-4 h-full">
             <div>
-              <Image src={contactImg} className="rounded-xl shadow-md" />
+              <Image
+                src={contactImg}
+                className="rounded-xl shadow-md"
+                alt="Contact Me"
+              />
             </div>
             <div>
               <h2 className="py-2">Alex Cheng</h2>
@@ -59,55 +95,85 @@ const Contact = () => {
         </div>
         <div className="col-span-3 w-full h-auto shadow-md shadow-gray-400 rounded-xl lg:p-4">
           <div className="p-4">
-            <form>
-              <div className="grid md:grid-cols-2 gap-4 w-full py-2">
-                <div className="flex flex-col">
-                  <label className="uppercase tracking-widest first-letter:text-sm py-2">
-                    Name
+            <div className="space-x-4 divide-x divide-gray-200 dark:divide-gray-700">
+              <form ref={form} onSubmit={sendEmail}>
+                {showAlert ? (
+                  <Alert
+                    color="info"
+                    withBorderAccent={true}
+                    icon={FaTelegramPlane}
+                    onDismiss={() => setShowAlert(false)}
+                  >
+                    <span className="font-normal">
+                      Message has successfully been sent.
+                    </span>
+                  </Alert>
+                ) : null}
+                {showError ? (
+                  <Alert
+                    color="failure"
+                    withBorderAccent={true}
+                    icon={HiInformationCircle}
+                    onDismiss={() => setShowError(false)}
+                  >
+                    <span className="font-normal">
+                      Something went wrong while sending the message.
+                    </span>
+                  </Alert>
+                ) : null}
+                <div className="grid md:grid-cols-2 gap-4 w-full py-2">
+                  <div className="flex flex-col">
+                    <label className="uppercase tracking-widest first-letter:text-sm py-2">
+                      Name
+                    </label>
+                    <input
+                      className="border-2 rounded-lg p-3 flex border-gray-300"
+                      type="text"
+                      name="name"
+                      required
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label className="uppercase tracking-widest text-sm py-2">
+                      E-Mail
+                    </label>
+                    <input
+                      className="border-2 rounded-lg p-3 flex border-gray-300"
+                      type="email"
+                      name="email"
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col py-2">
+                  <label className="uppercase tracking-widest text-sm py-2">
+                    Subject
                   </label>
                   <input
                     className="border-2 rounded-lg p-3 flex border-gray-300"
                     type="text"
+                    name="subject"
                     required
                   />
                 </div>
-                <div className="flex flex-col">
+                <div className="flex flex-col py-2">
                   <label className="uppercase tracking-widest text-sm py-2">
-                    E-Mail
+                    Message
                   </label>
-                  <input
-                    className="border-2 rounded-lg p-3 flex border-gray-300"
-                    type="email"
+                  <textarea
+                    className="border-2 rounded-lg p-3 border-gray-300"
+                    rows="8"
+                    type="text"
+                    resize="none"
+                    name="message"
                     required
                   />
                 </div>
-              </div>
-              <div className="flex flex-col py-2">
-                <label className="uppercase tracking-widest text-sm py-2">
-                  Subject
-                </label>
-                <input
-                  className="border-2 rounded-lg p-3 flex border-gray-300"
-                  type="text"
-                  required
-                />
-              </div>
-              <div className="flex flex-col py-2">
-                <label className="uppercase tracking-widest text-sm py-2">
-                  Message
-                </label>
-                <textarea
-                  className="border-2 rounded-lg p-3 border-gray-300"
-                  rows="10"
-                  type="text"
-                  resize="none"
-                  required
-                />
-              </div>
-              <button className="uppercase tracking-widest w-full mt-8 p-4">
-                Submit Message
-              </button>
-            </form>
+                <button className="uppercase tracking-widest w-full mt-8 p-4">
+                  Submit Message
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/**/*.js",
 
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -12,5 +13,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
