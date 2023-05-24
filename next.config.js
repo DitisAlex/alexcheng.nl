72530bd6350ddfc87cdb430a3ff8e1cad8c298be
Alex Cheng <alexcheng0314@gmail.com>
2023-05-24 11:13:27 +0200

    Revert to default config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: '_static',
-    images: {
-      unoptimized: true
-    }
+    reactStrictMode: true,
   };
-   
+  
   module.exports = nextConfig;
